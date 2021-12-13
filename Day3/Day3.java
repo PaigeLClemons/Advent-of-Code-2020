bd00702c038b9f8892a06d2693e70f0cd2779d38
PaigeLClemons <paigelc404@gmail.com>
2021-12-12 23:02:17 -0500

    Day 3 that's slightly less broken...maybe. This works with the example given on the website but not the given input.

--- Day3/Day3.java
+++ Day3/Day3.java
@@ -7,16 +7,16 @@ import java.util.ArrayList;
 public class Day3{
     public static void main(String[] args){
         try{
-            File inputFile = new File("Day3/test.txt");
+            File inputFile = new File("Day3/input.txt");
             Scanner scanner = new Scanner(inputFile);
             String gamma = "";
             String epsilon = "";
             ArrayList<String> binaryNums = new ArrayList<String>();
             ArrayList<String> binaryNums2 = new ArrayList<String>();
-            // int[] zeroCount = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-            // int[] oneCount = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-            int[] zeroCount = {0, 0, 0, 0, 0};
-            int[] oneCount = {0, 0, 0, 0, 0};
+            int[] zeroCount = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            int[] oneCount = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            // int[] zeroCount = {0, 0, 0, 0, 0};
+            // int[] oneCount = {0, 0, 0, 0, 0};
 
             while(scanner.hasNextLine()){
                 String temp = scanner.nextLine();
@@ -36,7 +36,7 @@ public class Day3{
                 }
             }
 
-            for(int i = 0; i < 5; i++){
+            for(int i = 0; i < 12; i++){
                 if(zeroCount[i] > oneCount[i]){
                     gamma += "0";
                     epsilon += "1";
@@ -53,23 +53,34 @@ public class Day3{
             //Gammma helps find Oxygen
             //Epsilon helps find CO2
 
-            for(int j = 0; j < gamma.length(); j++){
-                for(int i = 0; i < binaryNums.size(); i++){
-                    if(binaryNums.size() > 1){
-                        if(binaryNums.get(i).charAt(j) != gamma.charAt(j)){
-                            binaryNums.remove(i);
-                            i--;
+            int[] zeroCount2 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            int[] oneCount2 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            while(binaryNums.size() > 1){
+                for(int j = 0; j < gamma.length(); j++){
+                    for(int i = 0; i < binaryNums.size(); i++){
+                        if(binaryNums.get(i).charAt(j) == '0'){
+                            zeroCount2[j]++;
+                        }
+                        else{
+                            oneCount2[j]++;
                         }
                     }
-                }
-            }
 
-            for(int j = 0; j < epsilon.length(); j++){
-                for(int i = 0; i < binaryNums2.size(); i++){
-                    if(binaryNums2.size() > 1){
-                        if(binaryNums2.get(i).charAt(j) != epsilon.charAt(j)){
-                            binaryNums2.remove(i);
-                            i--;
+                    for(int i = 0; i < binaryNums.size(); i++){
+                        if(binaryNums.size() > 1){
+                            if(zeroCount2[j] != oneCount2[j]){
+                                if(binaryNums.get(i).charAt(j) != gamma.charAt(j)){
+                                    binaryNums.remove(i);
+                                    i--;
+                                }
+                            }
+                            else if(zeroCount2[j] == oneCount2[j]){
+                                if(binaryNums.get(i).charAt(j) == '0'){
+                                    binaryNums.remove(i);
+                                    i--;
+                                }
+                            }
                         }
                     }
                 }
@@ -77,9 +88,43 @@ public class Day3{
 
             System.out.println(binaryNums.size());
             System.out.println(binaryNums.get(0));
-           
 
-            //System.out.println("Life Support Rating = " + (Integer.parseInt(binaryNums.get(0), 2) * Integer.parseInt(binaryNums2.get(0), 2)));
+            int[] zeroCount3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            int[] oneCount3 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            while(binaryNums2.size() > 1){
+                for(int j = 0; j < epsilon.length(); j++){
+                    for(int i = 0; i < binaryNums2.size(); i++){
+                        if(binaryNums2.get(i).charAt(j) == '0'){
+                            zeroCount3[j]++;
+                        }
+                        else{
+                            oneCount3[j]++;
+                        }
+                    }
+                    for(int i = 0; i < binaryNums2.size(); i++){
+                        if(binaryNums2.size() > 1){
+                            if(zeroCount3[j] != oneCount3[j]){
+                                if(binaryNums2.get(i).charAt(j) != epsilon.charAt(j)){
+                                    binaryNums2.remove(i);
+                                    i--;
+                                }
+                            }
+                            else if(zeroCount3[j] == oneCount3[j]){
+                                if(binaryNums2.get(i).charAt(j) == '1'){
+                                    binaryNums2.remove(i);
+                                    i--;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            System.out.println(binaryNums2.size());
+            System.out.println(binaryNums2.get(0));           
+
+            System.out.println("Life Support Rating = " + (Integer.parseInt(binaryNums.get(0), 2) * Integer.parseInt(binaryNums2.get(0), 2)));
 
             
             scanner.close();
